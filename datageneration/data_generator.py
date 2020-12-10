e0f346df24793477400d9582e43ed967d4461e7f
ChenS676 <cshao676@gmail.com>
2020-12-11 00:42:29 +0100

    build single stream model half

--- datageneration/data_generator.py
+++ datageneration/data_generator.py
@@ -18,6 +18,8 @@ from IPython.display import clear_output
 import matplotlib.pyplot as plt
 from tensorflow.compat.v1 import ConfigProto
 from tensorflow.compat.v1 import InteractiveSession
+from models import single_stream
+
 
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 os.environ['CUDA_VISIBLE_DEVICES'] = "0"  # 选择哪一块gpu
@@ -86,30 +88,6 @@ def display(display_list):
     plt.show()
 
 
-def unet_model(output_channels):
-    inputs = tf.keras.layers.Input(shape=[128, 128, 3])
-    x = inputs
-
-    # Downsampling through the model
-    skips = down_stack(x)
-    x = skips[-1]
-    skips = reversed(skips[:-1])
-
-    # Upsampling and establishing the skip connections
-    for up, skip in zip(up_stack, skips):
-        x = up(x)
-        concat = tf.keras.layers.Concatenate()
-        x = concat([x, skip])
-
-    # This is the last layer of the model
-    last = tf.keras.layers.Conv2DTranspose(
-        output_channels, 3, strides=2,
-        padding='same')  # 64x64 -> 128x128
-
-    x = last(x)
-
-    return tf.keras.Model(inputs=inputs, outputs=x)
-
 
 def create_mask(pred_mask):
     pred_mask = tf.argmax(pred_mask, axis=-1)
@@ -149,37 +127,6 @@ class DisplayCallback(tf.keras.callbacks.Callback):
         show_predictions()
         print('\nSample Prediction after epoch {}\n'.format(epoch + 1))
 
-
-base_model = tf.keras.applications.MobileNetV2(input_shape=[128, 128, 3], include_top=False)
-
-# Use the activations of these layers
-layer_names = [
-    'block_1_expand_relu',  # 64x64
-    'block_3_expand_relu',  # 32x32
-    'block_6_expand_relu',  # 16x16
-    'block_13_expand_relu',  # 8x8
-    'block_16_project',  # 4x4
-]
-
-
-layers = [base_model.get_layer(name).output for name in layer_names]
-
-
-# Create the feature extraction model
-down_stack = tf.keras.Model(inputs=base_model.input, outputs=layers)
-
-
-down_stack.trainable = False
-
-
-up_stack = [
-    pix2pix.upsample(512, 3),  # 4x4 -> 8x8
-    pix2pix.upsample(256, 3),  # 8x8 -> 16x16
-    pix2pix.upsample(128, 3),  # 16x16 -> 32x32
-    pix2pix.upsample(64, 3),  # 32x32 -> 64x64
-]
-
-
 TRAIN_LENGTH = info.splits['train'].num_examples
 BATCH_SIZE = 1
 BUFFER_SIZE = 1000
@@ -197,11 +144,7 @@ test_dataset = test.batch(BATCH_SIZE)
 
 
 
-for image, mask in train.take(1):
-    sample_image, sample_mask = image, mask
-display([sample_image, sample_mask])
-
-model = unet_model(OUTPUT_CHANNELS)
+model = single_stream()
 model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
@@ -211,12 +154,6 @@ EPOCHS = 1
 VAL_SUBSPLITS = 5
 VALIDATION_STEPS = info.splits['test'].num_examples // BATCH_SIZE // VAL_SUBSPLITS
 
-# steps_per_epoch=STEPS_PER_EPOCH,
-# model_history = model.fit(train_dataset, epochs=EPOCHS,
-#                           steps_per_epoch=5,
-#                           validation_steps=VALIDATION_STEPS,
-#                           validation_data=test_dataset,
-#                           callbacks=[DisplayCallback()])
 
 train_loss_results = []
 train_accuracy_results = []
@@ -280,3 +217,65 @@ plt.legend()
 plt.show()
 
 show_predictions(test_dataset, 3)
+#
+# base_model = tf.keras.applications.MobileNetV2(input_shape=[128, 128, 3], include_top=False)
+#
+# # Use the activations of these layers
+# layer_names = [
+#     'block_1_expand_relu',  # 64x64
+#     'block_3_expand_relu',  # 32x32
+#     'block_6_expand_relu',  # 16x16
+#     'block_13_expand_relu',  # 8x8
+#     'block_16_project',  # 4x4
+# ]
+#
+#
+# layers = [base_model.get_layer(name).output for name in layer_names]
+#
+#
+# # Create the feature extraction model
+# down_stack = tf.keras.Model(inputs=base_model.input, outputs=layers)
+#
+#
+# down_stack.trainable = False
+#
+#
+# up_stack = [
+#     pix2pix.upsample(512, 3),  # 4x4 -> 8x8
+#     pix2pix.upsample(256, 3),  # 8x8 -> 16x16
+#     pix2pix.upsample(128, 3),  # 16x16 -> 32x32
+#     pix2pix.upsample(64, 3),  # 32x32 -> 64x64
+# ]
+#
+#
+
+#
+#
+# #
+# # for image, mask in train.take(1):
+# #     sample_image, sample_mask = image, mask
+# # display([sample_image, sample_mask])
+#
+# def unet_model(output_channels):
+#     inputs = tf.keras.layers.Input(shape=[128, 128, 3])
+#     x = inputs
+#
+#     # Downsampling through the model
+#     skips = down_stack(x)
+#     x = skips[-1]
+#     skips = reversed(skips[:-1])
+#
+#     # Upsampling and establishing the skip connections
+#     for up, skip in zip(up_stack, skips):
+#         x = up(x)
+#         concat = tf.keras.layers.Concatenate()
+#         x = concat([x, skip])
+#
+#     # This is the last layer of the model
+#     last = tf.keras.layers.Conv2DTranspose(
+#         output_channels, 3, strides=2,
+#         padding='same')  # 64x64 -> 128x128
+#
+#     x = last(x)
+#
+#     return tf.keras.Model(inputs=inputs, outputs=x)
