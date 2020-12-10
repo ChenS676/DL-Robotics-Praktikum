e0b174fb7a9292430ae6936d19d4b5e1d6fb6147
ChenS676 <cshao676@gmail.com>
2020-12-10 18:13:13 +0100

    build a easy trainer and generator for unet

--- datageneration/data_generator.py
+++ datageneration/data_generator.py
@@ -19,13 +19,12 @@ import matplotlib.pyplot as plt
 from tensorflow.compat.v1 import ConfigProto
 from tensorflow.compat.v1 import InteractiveSession
 
-
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-os.environ['CUDA_VISIBLE_DEVICES'] = "0"  #选择哪一块gpu
+os.environ['CUDA_VISIBLE_DEVICES'] = "0"  # 选择哪一块gpu
 config = ConfigProto()
-config.allow_soft_placement=True #如果你指定的设备不存在，允许TF自动分配设备
-config.gpu_options.per_process_gpu_memory_fraction=0.7  #分配百分之七十的显存给程序使用，避免内存溢出，可以自己调整
-config.gpu_options.allow_growth = True   #按需分配显存，这个比较重要
+config.allow_soft_placement = True  # 如果你指定的设备不存在，允许TF自动分配设备
+config.gpu_options.per_process_gpu_memory_fraction = 0.7  # 分配百分之七十的显存给程序使用，避免内存溢出，可以自己调整
+config.gpu_options.allow_growth = True  # 按需分配显存，这个比较重要
 session = InteractiveSession(config=config)
 
 # def gen():
@@ -43,161 +42,240 @@ session = InteractiveSession(config=config)
 # pip install -q -U tfds-nightly
 
 dataset, info = tfds.load('oxford_iiit_pet:3.*.*', with_info=True)
+
+
 def normalize(input_image, input_mask):
-  input_image = tf.cast(input_image, tf.float32) / 255.0
-  input_mask -= 1
-  return input_image, input_mask
+    input_image = tf.cast(input_image, tf.float32) / 255.0
+    input_mask -= 1
+    return input_image, input_mask
 
 
 @tf.function
 def load_image_train(datapoint):
-  input_image = tf.image.resize(datapoint['image'], (128, 128))
-  input_mask = tf.image.resize(datapoint['segmentation_mask'], (128, 128))
+    input_image = tf.image.resize(datapoint['image'], (128, 128))
+    input_mask = tf.image.resize(datapoint['segmentation_mask'], (128, 128))
 
-  if tf.random.uniform(()) > 0.5:
-    input_image = tf.image.flip_left_right(input_image)
-    input_mask = tf.image.flip_left_right(input_mask)
+    if tf.random.uniform(()) > 0.5:
+        input_image = tf.image.flip_left_right(input_image)
+        input_mask = tf.image.flip_left_right(input_mask)
 
-  input_image, input_mask = normalize(input_image, input_mask)
+    input_image, input_mask = normalize(input_image, input_mask)
 
-  return input_image, input_mask
+    return input_image, input_mask
+
 
 def load_image_test(datapoint):
-  input_image = tf.image.resize(datapoint['image'], (128, 128))
-  input_mask = tf.image.resize(datapoint['segmentation_mask'], (128, 128))
+    input_image = tf.image.resize(datapoint['image'], (128, 128))
+    input_mask = tf.image.resize(datapoint['segmentation_mask'], (128, 128))
 
-  input_image, input_mask = normalize(input_image, input_mask)
+    input_image, input_mask = normalize(input_image, input_mask)
 
-  return input_image, input_mask
+    return input_image, input_mask
 
-TRAIN_LENGTH = info.splits['train'].num_examples
-BATCH_SIZE = 64
-BUFFER_SIZE = 1000
-STEPS_PER_EPOCH = TRAIN_LENGTH // BATCH_SIZE
-OUTPUT_CHANNELS = 3
-
-train = dataset['train'].map(load_image_train, num_parallel_calls=tf.data.experimental.AUTOTUNE)
-test = dataset['test'].map(load_image_test)
-
-train_dataset = train.cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE).repeat()
-train_dataset = train_dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
-test_dataset = test.batch(BATCH_SIZE)
 
 def display(display_list):
-  plt.figure(figsize=(15, 15))
+    plt.figure(figsize=(15, 15))
 
-  title = ['Input Image', 'True Mask', 'Predicted Mask']
+    title = ['Input Image', 'True Mask', 'Predicted Mask']
 
-  for i in range(len(display_list)):
-    plt.subplot(1, len(display_list), i+1)
-    plt.title(title[i])
-    plt.imshow(tf.keras.preprocessing.image.array_to_img(display_list[i]))
-    plt.axis('off')
-  plt.show()
+    for i in range(len(display_list)):
+        plt.subplot(1, len(display_list), i + 1)
+        plt.title(title[i])
+        plt.imshow(tf.keras.preprocessing.image.array_to_img(display_list[i]))
+        plt.axis('off')
+    plt.show()
 
-for image, mask in train.take(1):
-  sample_image, sample_mask = image, mask
-display([sample_image, sample_mask])
+
+def unet_model(output_channels):
+    inputs = tf.keras.layers.Input(shape=[128, 128, 3])
+    x = inputs
+
+    # Downsampling through the model
+    skips = down_stack(x)
+    x = skips[-1]
+    skips = reversed(skips[:-1])
+
+    # Upsampling and establishing the skip connections
+    for up, skip in zip(up_stack, skips):
+        x = up(x)
+        concat = tf.keras.layers.Concatenate()
+        x = concat([x, skip])
+
+    # This is the last layer of the model
+    last = tf.keras.layers.Conv2DTranspose(
+        output_channels, 3, strides=2,
+        padding='same')  # 64x64 -> 128x128
+
+    x = last(x)
+
+    return tf.keras.Model(inputs=inputs, outputs=x)
+
+
+def create_mask(pred_mask):
+    pred_mask = tf.argmax(pred_mask, axis=-1)
+    pred_mask = pred_mask[..., tf.newaxis]
+    return pred_mask[0]
+
+
+def show_predictions(dataset=None, num=1):
+    if dataset:
+        for image, mask in dataset.take(num):
+            pred_mask = model.predict(image)
+            display([image[0], mask[0], create_mask(pred_mask)])
+    else:
+        display([sample_image, sample_mask,
+                 create_mask(model.predict(sample_image[tf.newaxis, ...]))])
+
+
+def loss(model, x, y, training):
+  # training=training is needed only if there are layers with different
+  # behavior during training versus inference (e.g. Dropout).
+  y_ = model(x, training=training)
+  # y_ = model(np.expand_dims(x, axis=0), training=training)
+  return tf.keras.losses.MSE(y_true=y, y_pred=y_)
+  # return tf.keras.losses.MSE(y_true=y, y_pred=np.expand_dims(y_, axis=0))
+
+
+def grad(model, inputs, targets):
+    with tf.GradientTape() as tape:
+        loss_value = loss(model, inputs, targets, training=True)
+    return loss_value, tape.gradient(loss_value, model.trainable_variables)
+
+
+# show_predictions()
+class DisplayCallback(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs=None):
+        clear_output(wait=True)
+        show_predictions()
+        print('\nSample Prediction after epoch {}\n'.format(epoch + 1))
+
 
 base_model = tf.keras.applications.MobileNetV2(input_shape=[128, 128, 3], include_top=False)
 
 # Use the activations of these layers
 layer_names = [
-    'block_1_expand_relu',   # 64x64
-    'block_3_expand_relu',   # 32x32
-    'block_6_expand_relu',   # 16x16
+    'block_1_expand_relu',  # 64x64
+    'block_3_expand_relu',  # 32x32
+    'block_6_expand_relu',  # 16x16
     'block_13_expand_relu',  # 8x8
-    'block_16_project',      # 4x4
+    'block_16_project',  # 4x4
 ]
+
+
 layers = [base_model.get_layer(name).output for name in layer_names]
+
 
 # Create the feature extraction model
 down_stack = tf.keras.Model(inputs=base_model.input, outputs=layers)
 
+
 down_stack.trainable = False
+
 
 up_stack = [
     pix2pix.upsample(512, 3),  # 4x4 -> 8x8
     pix2pix.upsample(256, 3),  # 8x8 -> 16x16
     pix2pix.upsample(128, 3),  # 16x16 -> 32x32
-    pix2pix.upsample(64, 3),   # 32x32 -> 64x64
+    pix2pix.upsample(64, 3),  # 32x32 -> 64x64
 ]
 
-def unet_model(output_channels):
-  inputs = tf.keras.layers.Input(shape=[128, 128, 3])
-  x = inputs
 
-  # Downsampling through the model
-  skips = down_stack(x)
-  x = skips[-1]
-  skips = reversed(skips[:-1])
+TRAIN_LENGTH = info.splits['train'].num_examples
+BATCH_SIZE = 1
+BUFFER_SIZE = 1000
+STEPS_PER_EPOCH = TRAIN_LENGTH // BATCH_SIZE
+OUTPUT_CHANNELS = 3
 
-  # Upsampling and establishing the skip connections
-  for up, skip in zip(up_stack, skips):
-    x = up(x)
-    concat = tf.keras.layers.Concatenate()
-    x = concat([x, skip])
 
-  # This is the last layer of the model
-  last = tf.keras.layers.Conv2DTranspose(
-      output_channels, 3, strides=2,
-      padding='same')  #64x64 -> 128x128
+train = dataset['train'].map(load_image_train, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+test = dataset['test'].map(load_image_test)
 
-  x = last(x)
 
-  return tf.keras.Model(inputs=inputs, outputs=x)
+train_dataset = train.cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE).repeat()
+train_dataset = train_dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+test_dataset = test.batch(BATCH_SIZE)
+
+
+
+for image, mask in train.take(1):
+    sample_image, sample_mask = image, mask
+display([sample_image, sample_mask])
 
 model = unet_model(OUTPUT_CHANNELS)
 model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
 
-tf.keras.utils.plot_model(model, show_shapes=True)
 
-def create_mask(pred_mask):
-  pred_mask = tf.argmax(pred_mask, axis=-1)
-  pred_mask = pred_mask[..., tf.newaxis]
-  return pred_mask[0]
-
-def show_predictions(dataset=None, num=1):
-  if dataset:
-    for image, mask in dataset.take(num):
-      pred_mask = model.predict(image)
-      display([image[0], mask[0], create_mask(pred_mask)])
-  else:
-    display([sample_image, sample_mask,
-             create_mask(model.predict(sample_image[tf.newaxis, ...]))])
-
-# show_predictions()
-
-class DisplayCallback(tf.keras.callbacks.Callback):
-  def on_epoch_end(self, epoch, logs=None):
-    clear_output(wait=True)
-    show_predictions()
-    print ('\nSample Prediction after epoch {}\n'.format(epoch+1))
-
-EPOCHS = 20
+EPOCHS = 1
 VAL_SUBSPLITS = 5
-VALIDATION_STEPS = info.splits['test'].num_examples//BATCH_SIZE//VAL_SUBSPLITS
+VALIDATION_STEPS = info.splits['test'].num_examples // BATCH_SIZE // VAL_SUBSPLITS
 
-model_history = model.fit(train_dataset, epochs=EPOCHS,
-                          steps_per_epoch=STEPS_PER_EPOCH,
-                          validation_steps=VALIDATION_STEPS,
-                          validation_data=test_dataset,
-                          callbacks=[DisplayCallback()])
+# steps_per_epoch=STEPS_PER_EPOCH,
+# model_history = model.fit(train_dataset, epochs=EPOCHS,
+#                           steps_per_epoch=5,
+#                           validation_steps=VALIDATION_STEPS,
+#                           validation_data=test_dataset,
+#                           callbacks=[DisplayCallback()])
 
-loss = model_history.history['loss']
-val_loss = model_history.history['val_loss']
+train_loss_results = []
+train_accuracy_results = []
+val_loss_results = []
+val_accuracy_results = []
 
-epochs = range(EPOCHS)
+optimizer = tf.keras.optimizers.Adam(learning_rate=0.00001)
+# loss_value, grads = grad(model, image, mask)
+# optimizer.apply_gradients(zip(grads, model.trainable_variables))
+num_epochs = 100
+
+for epoch in range(num_epochs):
+
+    train_image, train_mask = train_dataset.cache().as_numpy_iterator().next()
+    val_image, val_mask = test_dataset.cache().as_numpy_iterator().next()
+
+    epoch_loss_train = tf.keras.metrics.Mean(name="train_loss")
+    epoch_acc_train = tf.keras.metrics.SparseCategoricalAccuracy(name="train_acc")
+    epoch_loss_val = tf.keras.metrics.Mean(name="val_loss")
+    epoch_acc_val = tf.keras.metrics.SparseCategoricalAccuracy(name="val_acc")
+    # Training loop - using batches of 32
+    # Optimize the model
+
+    loss_value, grads = grad(model, train_image, train_mask)
+    optimizer.apply_gradients(zip(grads, model.trainable_variables))
+
+    # Track progress
+    epoch_loss_train.update_state(loss_value)  # Add current batch loss
+    # Compare predicted label to actual label
+    # training=True is needed only if there are layers with different
+    # behavior during training versus inference (e.g. Dropout).
+    epoch_acc_train.update_state(train_mask, model(train_image, training=True))
+
+    # End epoch
+    train_loss_results.append(epoch_loss_train.result().numpy())
+    train_accuracy_results.append(epoch_acc_train.result().numpy())
+
+    epoch_loss_val.update_state(loss(model, val_image, val_mask, True))
+    epoch_acc_val.update_state(val_mask, model(val_image, training=True))
+
+    val_loss_results.append(epoch_loss_train.result().numpy())
+    val_accuracy_results.append(epoch_acc_val.result().numpy())
+
+
+    if epoch % 5 == 0:
+        print("Epoch {:03d}: Loss: {:.3f}, Accuracy: {:.3%}".format(epoch,
+                                                                    epoch_loss_train.result(),
+                                                                    epoch_acc_train.result()))
+
+
+epochs = range(num_epochs)
 
 plt.figure()
-plt.plot(epochs, loss, 'r', label='Training loss')
-plt.plot(epochs, val_loss, 'bo', label='Validation loss')
+plt.plot(epochs, train_loss_results, 'r', label='Training loss')
+plt.plot(epochs, val_loss_results, 'bo', label='Validation loss')
 plt.title('Training and Validation Loss')
 plt.xlabel('Epoch')
 plt.ylabel('Loss Value')
-plt.ylim([0, 1])
+plt.ylim([0, 2])
 plt.legend()
 plt.show()
 
